c4e9c7fda15c2739a5189b54fd88870fab1eb0b6
8HaYe8 <90028574+8HaYe8@users.noreply.github.com>
2021-09-17 12:31:24 +0300

    Add files via upload

---
+++ prac_1.py
@@ -0,0 +1,38 @@
+print("ex1-2")
+print("Hello, world!")
+print("What's up?")
+print("I'm a little bit sad(")
+print("Why? What's happen?")
+print("ex3")
+x=int(input("length"))
+y=int(input("width"))
+S=x*y
+print("S=", S)
+print("ex4")
+x1=int(input())
+x2=int(input())
+s=x1+x2
+m=x1-x2
+n=x1*x2
+b=x1/x2
+print("sum=", s)
+print("dif=", m)
+print("mul=", n)
+print("quot=", b)
+print("ex5")
+r=int(input("radius"))
+p=3.14159
+print("Diameter=", r*2,
+      "Circumference=", 2*r*p,
+      "Area=", p*r**2)
+print("ex6")
+i=int(input("3digit number"))
+m1=i//100
+m2=i//10%10
+m3=i%10
+v=m1+m2+m3
+v1=m1-m2-m3
+v2=m1*m2*m3
+print("Sum=", v)
+print("Dif=", v1)
+print("Mul=", v2)
